3b5b7b1acc2549e20c3419e12ac4bfefcef9d274
RanaNasir018 <77691146+RanaNasir018@users.noreply.github.com>
2022-11-17 14:38:56 +0500

    working..

--- src/screens/auth/login/index.js
+++ src/screens/auth/login/index.js
@@ -1,22 +1,91 @@
-import React from "react";
-import { FlatList, Text, View } from "react-native";
+import React, { useState } from "react";
+import { FlatList, Text, View, Image, TouchableOpacity, } from "react-native";
 import styles from "./styles";
+import Modal from "react-native-modal"
 import { useDispatch } from "react-redux";
 import { Button, ScreenWrapper } from "~components";
 import { setIsLoggedIn, setUserMeta } from "~redux/slices/user";
 import { setAppLoader } from "~redux/slices/config";
+import Entypo from "react-native-vector-icons/Entypo";
 import CustomCalender from "~components/CustomCalender";
 import dayjs from "dayjs";
+import { height, width } from "~utills/Dimension";
+var localeData = require("dayjs/plugin/localeData");
+var weekday = require("dayjs/plugin/weekday");
+dayjs.extend(localeData);
+dayjs.extend(weekday);
 export default function Login({ navigation, route }) {
   const dispatch = useDispatch();
-  const getCurrentMonth = dayjs().format("MMMM").valueOf()
-  const getCurrentYear = dayjs().format("YYYY").valueOf()
-  const headerData = `${getCurrentMonth} ${getCurrentYear}`
-  console.log(headerData);
+  let dayOfWeek = [];
+  let daysstring = [];
+  daysstring = dayjs.weekdaysShort();
+  for (let i = 0; i < 7; i++) {
+    const date = dayjs().day(i).date();
+    dayOfWeek.push(date);
+  }
+  const getCurrentMonth = dayjs().format("MMMM").valueOf();
+  const getCurrentYear = dayjs().format("YYYY").valueOf();
+  const headerData = `${getCurrentMonth} ${getCurrentYear}`;
+  const currentDate = dayjs().date()
+  const [isVisible, setIsVisible] = useState(false)
+  const CalenderModal = () => {
+    return (
+
+      <Modal
+        isVisible={isVisible}
+        style={{ alignItems: "center", backgroundColor: "#FFFFFF" }}
+      >
+
+        <CustomCalender />
+        <TouchableOpacity onPress={() => setIsVisible(false)}>
+          <Text style={{ color: "red", fontSize: height(2), marginTop: height(2) }}>Close</Text>
+        </TouchableOpacity>
+      </Modal>
+
+
+
+    )
+  }
   return (
     <ScreenWrapper>
       <View style={styles.mainViewContainer}>
-        <FlatList />
+        <CalenderModal />
+        <View style={styles.calendarContainer}>
+          <View style={styles.headerWrapper}>
+            <Text style={styles.headerMonth}>{headerData}</Text>
+            <TouchableOpacity onPress={() => setIsVisible(true)} style={styles.iconWrapper}>
+              <Image
+                source={require("../../../assets/Icons/down.png")}
+                resizeMode="contain"
+                style={styles.IconStyle}
+              />
+            </TouchableOpacity>
+          </View>
+          <View style={styles.bottomSection}>
+            <View style={styles.days}>
+              {daysstring.map((item, index) => {
+                return (
+                  <View key={index}>
+                    <Text style={styles.daysText}>{item}</Text>
+                  </View>
+                )
+              })}
+            </View>
+            <View style={styles.dates}>
+              {dayOfWeek.map((item, index) => {
+                return (
+                  <TouchableOpacity key={index} style={[styles.datesBoxes, currentDate == item && { backgroundColor: "#62BDBE" }]}>
+                    <Text style={[styles.datestxt, currentDate == item && { color: "#FFFFFF" }]}>{item}</Text>
+                  </TouchableOpacity>
+                )
+              })}
+            </View>
+
+          </View>
+          <View>
+
+          </View>
+        </View>
         {/* <Text style={styles.title}>LOGIN SCREEN</Text>
         <Button
           title={"Login"}
--- src/screens/auth/login/styles.js
+++ src/screens/auth/login/styles.js
@@ -15,6 +15,71 @@ const styles = StyleSheet.create({
     fontSize: width(4),
     marginBottom: height(2)
   },
+  //calender days
+  calendarContainer: {
+    height: height(15),
+    backgroundColor: "#FFFFFF",
+    width: width(90),
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 4,
+    borderRadius: width(3)
+  },
+  headerWrapper: {
+    flexDirection: "row",
+    marginVertical: height(1.5),
+    marginHorizontal: width(4),
+    justifyContent: "space-between",
+    alignItems: "center"
+  },
+  headerMonth: {
+    color: "#273D52",
+    fontSize: width(3.5),
+    fontWeight: "500"
+  },
+  IconStyle: {
+    height: height(2.5),
+    width: width(5)
+  },
+  iconWrapper: {
 
+  },
+  bottomSection: {
+    height: height(9),
+    marginHorizontal: width(4),
+  },
+  days: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    paddingHorizontal: width(1.8)
+  },
+  dates: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    marginTop: height(1)
+  },
+  daysText: {
+    color: "#273D52",
+    fontSize: width(3)
+  },
+  datesBoxes: {
+    backgroundColor: "#F6F8F9",
+    height: height(4),
+    width: height(4),
+    borderRadius: height(2),
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  datestxt: {
+    fontSize: width(3.2),
+    color: "#273D52"
+  }
 });
 export default styles;
